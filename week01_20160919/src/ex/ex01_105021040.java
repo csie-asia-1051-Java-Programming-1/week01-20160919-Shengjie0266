942ac44c3a2e7a63fbe515c1b484937ca990a91e
Shengjie0266 <p124110266>
2016-09-19 15:55:39 +0800

    ex
    lesson ex

---
+++ week01_20160919/src/ex/ex01_105021040.java
@@ -0,0 +1,18 @@
+package ex;
+/*
+ * Topic: 隢神銝��蝔�霈蝙�刻�頛詨�瘞�皞怠漲嚗撓�箝����澈摨�(�舀� = ��*(9/5)+32)
+ * Date: 2016/09/19
+ * Author: 1050210XX �冽偶�航�撣�
+ */
+import java.util.Scanner;
+public class ex01_105021040 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner a = new Scanner(System.in);
+		int b = a.nextInt();
+		int c = (b-32)*5/9;
+		System.out.println("攝氏為:"+c);
+	}
+
+}
--- week01_20160919/src/ex/ex02_105021040.java
+++ week01_20160919/src/ex/ex02_105021040.java
@@ -4,12 +4,15 @@ package ex;
  * Date: 2016/09/19
  * Author: 1050210XX 周永振老師
  */
-
-public class ex02_1050210XX {
+import java.util.Scanner;
+public class ex02_105021040 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner a = new Scanner(System.in);
+		int b = a.nextInt();
+		double c = b/0.45359;
+		System.out.println("體重為:"+ c);
 	}
 
 }
